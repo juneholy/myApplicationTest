8d15e514599f329d8c5d4658fcd234b567ac7214
houlin.jiang <houlin.jiang@qunar.com>
2016-05-26 18:43:11 +0800

    koad

--- MyApplicationTest/app/src/main/java/com/example/houlinjiang/myapplicationtest/web/LoadActivity.java
+++ MyApplicationTest/app/src/main/java/com/example/houlinjiang/myapplicationtest/web/LoadActivity.java
@@ -2,16 +2,19 @@ package com.example.houlinjiang.myapplicationtest.web;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
+import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.houlinjiang.myapplicationtest.R;
 
@@ -21,17 +24,22 @@ import com.example.houlinjiang.myapplicationtest.R;
 public class LoadActivity extends Activity {
 
     private WebView webView;
-
+    String url = "https://ss0.bdstatic.com/l4oZeXSm1A5BphGlnYG/skin/";
+    int base = 400;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().requestFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.activity_load);
         webView = (WebView) findViewById(R.id.load_webview);
+        webView.setInitialScale(110);
         WebSettings settings = webView.getSettings();
         settings.setSupportZoom(true);
         settings.setBuiltInZoomControls(true);
         settings.setJavaScriptEnabled(true);
+//        settings.setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
+//        settings.setLoadWithOverviewMode(true);
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -54,22 +62,39 @@ public class LoadActivity extends Activity {
 
         webView.setWebChromeClient(new WebChromeClient() {
             //当WebView进度改变时更新窗口进度
-                    @Override
+            @Override
             public void onProgressChanged(WebView view, int newProgress) {
                 //Activity的进度范围在0到10000之间,所以这里要乘以100
                 LoadActivity.this.setProgress(newProgress * 100);
-                }
-           });
-        final EditText url = (EditText) findViewById(R.id.url);
+            }
+        });
+        final TextView tv = (TextView) findViewById(R.id.url);
         Button loadURL = (Button) findViewById(R.id.loadURL);
-       loadURL.setOnClickListener(new View.OnClickListener() {
+        loadURL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               webView.loadUrl(url.getText().toString()); //加载url
+                String loadUrl = url + (base++)+".jpg";
+                tv.setText(loadUrl);
+                webView.loadUrl(loadUrl); //加载url
+                webView.clearHistory();
                 webView.requestFocus(); //获取焦点
-                }
-            });
+            }
+        });
 
     }
 
+    @Override
+    public void onBackPressed() {
+        this.finish();
+//        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
 }
